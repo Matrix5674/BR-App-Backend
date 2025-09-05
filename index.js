4f8704aecb6b83c532f2374ec387a9e4299e8d6c
Soham <sohamsah28@gmail.com>
2025-09-05 10:01:51 -0700

    update

--- index.js
+++ index.js
@@ -1,26 +1,33 @@
 import admin from 'firebase-admin';
 import express from 'express';
 import cors from 'cors';
-import serviceAccount from './service_account.json' assert { type:'json' };
+import dotenv from "dotenv";
+
 
 import { authenticate } from './Middleware/auth';
 
+const PORT = process.env.PORT || 4000;
+
+dotenv.config();
+
+const serviceAccount = JSON.parse(process.env.SERVICEACCOUNT);
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
 });
 
 const app = express();
+
 app.use(cors());
 app.use(express.json());
 
-app.listen(4000, () => console.log("Server running on port 4000"));
+app.listen(PORT, () => console.log(`Server running on port ${4000}`));
 
 app.get("/user", authenticate, async (req, res) => {
     try {
         const uid = req.user.uid;
         const userRecord = await admin.auth().getUser(uid);
-        const role = req.user.role || 'user'; // Default role is 'user' if not specified
+        const role = req.user.role || 'user'; 
 
         res.json({
             uid: userRecord.uid,
