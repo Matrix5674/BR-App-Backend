cc44e638e893e1177701c655471a8d5d0ac131d6
Soham <sohamsah28@gmail.com>
2025-09-05 12:56:13 -0700

    import fix

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import cors from 'cors';
 import dotenv from "dotenv";
 
 
-import { authenticate } from './Middleware/auth';
+import { authenticate } from './Middleware/auth.js';
 
 const PORT = process.env.PORT || 4000;
 
